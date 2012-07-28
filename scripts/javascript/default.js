95c3301826ed909ccf9c2bb1aeb4e5f54d603ad8
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-28 15:06:08 -0400

    Initial Commit
    Initial commit of initial typeset directories and files

---
+++ scripts/javascript/default.js
@@ -0,0 +1,9 @@
+﻿﻿/// <reference path="../base/jquery-1.7.2-vsdoc.js" />
+$(document).ready(function () {
+    //Apply twitter Bootstrap classes
+    $('body').addClass('container');
+    $('body > header, body > #content, body > footer').wrapAll('<section class="row-fluid" />');
+    $('body > .row-fluid > header').addClass('span2');
+    $('body > .row-fluid > #content').addClass('span8');
+    $('body > .row-fluid > footer').addClass('span12');
+});
