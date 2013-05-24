0fa8cdc14e22d13ca0b73485e6209642eb0432a3
Bates Westmoreland <rbwestmoreland@gmail.com>
2013-05-24 17:19:18 -0400

    wait for prettyPrint to load, before running tabs to spaces

--- scripts/javascript/default.js
+++ scripts/javascript/default.js
@@ -1,13 +1,13 @@
 ﻿﻿/// <reference path="../base/jquery-1.7.2-vsdoc.js" />
 $(document).ready(function () {
-	app.init();
+    app.init();
 });
 
 window.app = window.app || {};
 //twitter bootstrap
 app.bootstrap = app.bootstrap || {};
 app.bootstrap.apply = function() {
-	$('body').addClass('container');
+    $('body').addClass('container');
     $('body > header, body > #content, body > footer').wrapAll('<section class="row-fluid" />');
     $('body > .row-fluid > header').addClass('span2');
     $('body > .row-fluid > #content').addClass('span10');
@@ -16,13 +16,19 @@ app.bootstrap.apply = function() {
 //google code prettify
 app.prettyPrint = app.prettyPrint || {};
 app.prettyPrint.apply = function() {
-	$('pre').addClass('prettyprint');
-	$('head').append('<script src="//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js"></'+'script>');
-	$('head').append('<link href="//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.css" rel="stylesheet" type="text/css">');
-	prettyPrint();
+    $('pre').addClass('prettyprint');
+    $('head').append('<script src="//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js"></'+'script>');
+    $('head').append('<link href="//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.css" rel="stylesheet" type="text/css">');
+    app.prettyPrint.run();  
+};
+app.prettyPrint.run = function() {
+    if(prettyPrint)
+        prettyPrint();
+    else
+        setTimeout(app.prettyPrint.run, 100);
 };
 
 app.init = function() {
-	app.bootstrap.apply();
-	app.prettyPrint.apply();
+    app.bootstrap.apply();
+    app.prettyPrint.apply();
 };
