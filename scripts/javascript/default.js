2d8a45ed487183564b6c62ed32eef134e891ebd5
Bates Westmoreland <rbwestmoreland@gmail.com>
2013-05-24 17:06:58 -0400

    refactored default.js added google code prettify

--- scripts/javascript/default.js
+++ scripts/javascript/default.js
@@ -1,9 +1,28 @@
 ﻿﻿/// <reference path="../base/jquery-1.7.2-vsdoc.js" />
 $(document).ready(function () {
-    //Apply twitter Bootstrap classes
-    $('body').addClass('container');
+	app.init();
+});
+
+window.app = window.app || {};
+//twitter bootstrap
+app.bootstrap = app.bootstrap || {};
+app.bootstrap.apply = function() {
+	$('body').addClass('container');
     $('body > header, body > #content, body > footer').wrapAll('<section class="row-fluid" />');
     $('body > .row-fluid > header').addClass('span2');
     $('body > .row-fluid > #content').addClass('span10');
     $('body > .row-fluid > footer').addClass('span12');
-});
+};
+//google code prettify
+app.prettyPrint = app.prettyPrint || {};
+app.prettyPrint.apply = function() {
+	$('pre').addClass('prettyprint');
+	$('head').append('<script src="//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js"></'+'script>');
+	$('head').append('<link href="//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.css" rel="stylesheet" type="text/css">');
+	prettyPrint();
+};
+
+app.init = function() {
+	app.bootstrap.apply();
+	app.prettyPrint.apply();
+};
